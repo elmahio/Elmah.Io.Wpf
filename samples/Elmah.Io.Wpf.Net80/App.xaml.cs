2ab818fe957cf3d75e0d687d44af1fb225e168b8
Thomas Ardal <thomasardal@gmail.com>
2023-11-21 11:18:16 +0100

    .NET 8 sample

---
+++ samples/Elmah.Io.Wpf.Net80/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace Elmah.Io.Wpf.Net80
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            ElmahIoWpf.Init(new ElmahIoWpfOptions
+            {
+                ApiKey = "API_KEY",
+                LogId = new Guid("LOG_ID"),
+                Application = "WPF on .NET 8",
+
+                // Use the OnFilter action to ignore specific log messages
+                //OnFilter = msg =>
+                //{
+                //    return msg.Type.Equals("System.NullReferenceException");
+                //},
+
+                // Use the OnMessage action to decorate all log messages
+                //OnMessage = msg =>
+                //{
+                //    msg.Version = "8.0.0";
+                //},
+            });
+        }
+    }
+
+}
