2f3aa82f2e3474d8ebd994873370108b5d7fc893
Thomas Ardal <thomasardal@gmail.com>
2024-11-18 10:29:44 +0100

    Build, test and sample for .NET 9

---
+++ samples/Elmah.Io.Wpf.Net90/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Elmah.Io.Wpf.Net90
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S125:Sections of code should not be commented out", Justification = "For test purposes only")]
+        public App()
+        {
+            ElmahIoWpf.Init(new ElmahIoWpfOptions
+            {
+                ApiKey = "API_KEY",
+                LogId = new Guid("LOG_ID"),
+                Application = "WPF on .NET 9",
+
+                // Use the OnFilter action to ignore specific log messages
+                //OnFilter = msg =>
+                //{
+                //    return msg.Type.Equals("System.NullReferenceException");
+                //},
+
+                // Use the OnMessage action to decorate all log messages
+                //OnMessage = msg =>
+                //{
+                //    msg.Version = "9.0.0";
+                //},
+            });
+        }
+    }
+}
---
+++ samples/Elmah.Io.Wpf.Net90/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Elmah.Io.Client;
+using System.Windows;
+
+namespace Elmah.Io.Wpf.Net90
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBreadcrumbBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ElmahIoWpf.AddBreadcrumb(new Breadcrumb(DateTime.UtcNow, "Warning", "Warning", "I tell you, ever since he got that Master Control Program..."));
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S112:General or reserved exceptions should never be thrown", Justification = "For test purposes only")]
+        private void ThrowExceptionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            throw new ApplicationException("The system's got more bugs than a bait store");
+        }
+    }
+}
